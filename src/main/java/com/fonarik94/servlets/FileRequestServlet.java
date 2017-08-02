8502238480e25bd6ad660cc403dcb12c676f0d6c
Fonarik94 <fonarik94@gmail.com>
2017-08-02 17:17:37 +0300

    Minor optimization of downloading files

--- src/main/java/com/fonarik94/servlets/FileRequestServlet.java
+++ src/main/java/com/fonarik94/servlets/FileRequestServlet.java
@@ -17,31 +17,21 @@ import java.util.Arrays;
 public class FileRequestServlet extends HttpServlet {
     private static final Logger logger = LogManager.getLogger(getCurentClassName());
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        String fileURI = "WEB-INF/classes" + request.getRequestURI().replaceFirst("/resources", "");
+        fileURI = fileURI.replace("/", File.separator);
 
-        StringBuilder pathToRequestedFile = new StringBuilder();
-        pathToRequestedFile.append(getServletContext().getRealPath(File.separator)).append(File.separator)
-                .append("WEB-INF").append(File.separator).append("classes").append(File.separator);
-        String[] requestedFile = request.getRequestURI().split("/");
-        for (int i = 2; i < requestedFile.length; i++) {
-            pathToRequestedFile.append(requestedFile[i]).append(File.separator);
+        response.setContentType(getServletContext().getMimeType(fileURI));
+        try (InputStream requestedFileInputStream = getServletContext().getResourceAsStream(fileURI);
+             InputStream bufferedFileInputStream = new BufferedInputStream(requestedFileInputStream);
+             OutputStream responseOutputStream = new BufferedOutputStream(response.getOutputStream())) {
+            int val;
+            while ((val = bufferedFileInputStream.read()) != -1) {
+                responseOutputStream.write(val);
+            }
+            responseOutputStream.flush();
+        } catch (IOException e) {
+            logger.error(">> " + e.toString());
         }
-        pathToRequestedFile.deleteCharAt(pathToRequestedFile.length() - 1);
-        String normalPath = pathToRequestedFile.toString().replaceAll("\\\\", "\\\\\\\\");
-        response.setContentType(getServletContext().getMimeType(requestedFile[requestedFile.length - 1]));
-
-        logger.debug(requestedFile[requestedFile.length - 1] + "MIME type is " + getServletContext().getMimeType(requestedFile[requestedFile.length - 1]));
-
-
-        BufferedInputStream bufferedFileInputStream = new BufferedInputStream(new FileInputStream(normalPath));
-        BufferedOutputStream responseOutputStream = new BufferedOutputStream(response.getOutputStream());
-
-        int val;
-        while ((val = bufferedFileInputStream.read()) != -1) {
-            responseOutputStream.write(val);
-        }
-        bufferedFileInputStream.close();
-        responseOutputStream.flush();
-        responseOutputStream.close();
     }
 }
